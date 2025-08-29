adf4d521544f3dacaec884d63edb792bec2d5fff
João Henrique <joaohscorrea@gmail.com>
2025-08-28 21:59:55 -0300

    alterações no projeto

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+
+	"list"
+)
+
+func main() {
+	//var arraylist list.ArrayList
+	arraylist := list.ArrayList{}
+	limit := 15
+	arraylist.Init(limit)
+
+	for i := 0; i < limit; i++ {
+		arraylist.Add(i)
+	}
+	for i := 0; i < limit; i++ {
+		val, err := arraylist.Get(i)
+		if err == nil {
+			fmt.Println(val)
+		}
+	}
+	for i := 0; i < limit; i++ {
+		if i%2 == 0 {
+			arraylist.Set(i*2, i)
+		}
+	}
+	fmt.Println("######################")
+	for i := 0; i < limit; i++ {
+		val, err := arraylist.Get(i)
+		if err == nil {
+			fmt.Println(val)
+		}
+	}
+}
